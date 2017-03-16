7dc0265eee65c8327761af8af21ff39d0572fd29
junnplus <junnplus@gmail.com>
2017-03-16 22:39:09 +0800

    Relative Ranks

---
+++ relative-ranks.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findRelativeRanks(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        RANK = ['Gold Medal', 'Silver Medal', 'Bronze Medal']
+        sorted_nums = sorted(nums)
+        ret = []
+        for n in nums:
+            i = len(nums) - sorted_nums.index(n)
+            if i <= 3:
+                ret.append(RANK[i - 1])
+            else:
+                ret.append(str(i))
+        return ret
+
+
+if __name__ == '__main__':
+    nums = [6, 3, 1, 4, 5, 7, 9]
+    print nums
+    s = Solution().findRelativeRanks(nums)
+    assert s == ['Bronze Medal', '6', '7', '5', '4', 'Silver Medal', 'Gold Medal']
